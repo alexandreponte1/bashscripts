0b412b7e27064d04640a1437129ae8d890f64513
Alexandre Ponte <uaspont@gmail.com>
2022-11-07 18:50:52 -0300

    Update manterversoes.sh

--- GCP/manterversoes.sh
+++ GCP/manterversoes.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-#aqui estou vendo quantas versões existem
-version=$(gcloud secrets versions list nomedoSecret --project idProjeto | grep enabled |sort -n | awk '{print$1}' | wc -l)
+SECRETID="nomedoSecret"
+PROJECTID="IDPROJETO"
 
+
+
+#aqui estou vendo quantas versões existem
+version=$(gcloud secrets versions list $SECRETID --project $PROJECTID | grep enabled |sort -n | awk '{print$1}' | wc -l)
 #apenas um contador
 versao=1
 
 #aqui estou pegando o número da penultima versão pra manter apenas 2
-manter=$(gcloud secrets versions list nomedoSecret --project idProjeto | head -3 | tail -1| awk '{print$1}')
+manter=$(gcloud secrets versions list $SECRETID --project $PROJECTID | head -3 | tail -1| awk '{print$1}')
 
 #echo nas variaveis
 echo $version
@@ -16,7 +20,7 @@ echo $versao
 
 while [ $versao -lt $manter ]
 do
-  gcloud secrets versions destroy $versao --secret=test123 --project active-chimera-359118 --quiet
+  gcloud secrets versions destroy $versao --secret=$SECRETID --project $PROJECTID --quiet
   versao=$[$versao +1]
 done
 
